c046be8966b1b4eb8459cb78e91b020102de5f17
bruno-holanda15 <br.holanda15@gmail.com>
2019-11-28 20:25:55 -0300

    index

--- index.php
+++ index.php
@@ -35,13 +35,12 @@
                         </div>
                     </div>
                     </div>
-                        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
+                        <a class="carousel-control-prev" href="#sliderHome" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
                         </a>
-                        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
+                        <a class="carousel-control-next" href="#sliderHome" role="button" data-slide="next">
+                        
                         </a>
                 </div>
             </div>
