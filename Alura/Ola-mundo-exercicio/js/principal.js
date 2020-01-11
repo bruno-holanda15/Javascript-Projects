de2df782b3b68a0cd633f153c2909c87f44de2b8
bruno-holanda15 <br.holanda15@gmail.com>
2020-01-11 18:56:24 -0300

    Adicionando novas práticas do Curso

--- Alura/Ola-mundo-exercicio/js/principal.js
+++ Alura/Ola-mundo-exercicio/js/principal.js
@@ -1,11 +1,29 @@
-	// console.log("Olá");
-    alert("hello");
-    // boa pratica utilizar classes e ids nos selectores 
-    var titulo = (document.querySelector(".titulo"));
+// boa pratica utilizar classes e ids nos selectores 
+var titulo = (document.querySelector(".titulo"));
 
-    console.log(titulo);
-    console.log(titulo.textContent);
+// console.log(titulo);
+// console.log(titulo.textContent);
 
-    titulo.textContent = "Banana é bom";
+titulo.textContent = "Banana é muito bom";
 
-    
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imcResultado = ( peso )/(Math.pow( altura ,2));
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imcResultado;
+
+
+
+console.log(paciente); // conter o tr 
+console.log(tdPeso); // devolver td
+console.log("O peso é : " + peso); // devolver somente o peso
+console.log("A altura é : " + altura);// devolver somente o altura
+console.log("O IMC é : " + imcResultado);// devolver somente o imc
+
+
