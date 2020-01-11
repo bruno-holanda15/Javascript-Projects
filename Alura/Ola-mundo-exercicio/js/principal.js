c9241967ec59747b1ea3cbe789b27e1bf433d3fc
bruno-holanda15 <br.holanda15@gmail.com>
2020-01-11 14:21:53 -0300

    Ajustando pastas de js e função querySelector por classes,  boa prática de estruturar o js

---
+++ Alura/Ola-mundo-exercicio/js/principal.js
@@ -0,0 +1,11 @@
+	// console.log("Olá");
+    alert("hello");
+    // boa pratica utilizar classes e ids nos selectores 
+    var titulo = (document.querySelector(".titulo"));
+
+    console.log(titulo);
+    console.log(titulo.textContent);
+
+    titulo.textContent = "Banana é bom";
+
+    
