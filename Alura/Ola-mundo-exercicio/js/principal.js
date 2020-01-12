42900d6aad2491267967461c6cdfd2d820dff74a
bruno-holanda15 <br.holanda15@gmail.com>
2020-01-12 12:12:12 -0300

    Adicionando validação para campos que estão no HTML por condicionais no js

--- Alura/Ola-mundo-exercicio/js/principal.js
+++ Alura/Ola-mundo-exercicio/js/principal.js
@@ -13,17 +13,35 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imcResultado = ( peso )/(Math.pow( altura ,2));
+var pesoValido = true;
+var alturaValida = true;
 
-var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imcResultado;
+if(peso <= 0 || peso >= 200){
+    console.log("Peso é inválido");
+    pesoValido = false;
+    tdPeso.textContent = "Peso inválido";
+}
+
+if(altura <= 0 || altura >= 3){
+    console.log("Altura é inválida");
+    alturaValida = false;
+    tdAltura.textContent = "Altura inválido";
 
 
+}
 
-console.log(paciente); // conter o tr 
-console.log(tdPeso); // devolver td
-console.log("O peso é : " + peso); // devolver somente o peso
-console.log("A altura é : " + altura);// devolver somente o altura
-console.log("O IMC é : " + imcResultado);// devolver somente o imc
+if(alturaValida  && pesoValido ){
+
+        var imcResultado = ( peso )/(Math.pow( altura ,2));
+
+        var tdImc = paciente.querySelector(".info-imc");
+        tdImc.textContent = imcResultado;
+
+}
+// console.log(paciente); // conter o tr 
+// console.log(tdPeso); // devolver td
+// console.log("O peso é : " + peso); // devolver somente o peso
+// console.log("A altura é : " + altura);// devolver somente o altura
+// console.log("O IMC é : " + imcResultado);// devolver somente o imc
 
 
