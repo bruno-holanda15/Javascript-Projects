fbda29d9e1df092b3f6ca631c3ffd6743388c299
bruno-holanda15 <br.holanda15@gmail.com>
2020-01-12 12:45:48 -0300

    Adicionando looping para validar todos os pacientes

--- Alura/Ola-mundo-exercicio/js/principal.js
+++ Alura/Ola-mundo-exercicio/js/principal.js
@@ -6,42 +6,50 @@ var titulo = (document.querySelector(".titulo"));
 
 titulo.textContent = "Banana é muito bom";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+var pacientes = document.querySelectorAll(".paciente");// querySelectorAll seleciona todas as tags com classe paciente e joga em um array
+console.log(pacientes);
 
-var pesoValido = true;
-var alturaValida = true;
+for (let i = 0; i < pacientes.length; i++) {
 
-if(peso <= 0 || peso >= 200){
-    console.log("Peso é inválido");
-    pesoValido = false;
-    tdPeso.textContent = "Peso inválido";
+    var paciente = pacientes[i];    
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if(peso <= 0 || peso >= 200){
+        console.log("Peso é inválido");
+        pesoValido = false;
+        tdPeso.textContent = "Peso inválido";
+    }
+
+    if(altura <= 0 || altura >= 3){
+        console.log("Altura é inválida");
+        alturaValida = false;
+        tdAltura.textContent = "Altura inválido";
+
+
+    }
+
+    if(alturaValida  && pesoValido ){
+
+            var imcResultado = ( peso )/(Math.pow( altura ,2));
+
+            var tdImc = paciente.querySelector(".info-imc");
+            tdImc.textContent = imcResultado.toFixed(2);
+
+    }
 }
-
-if(altura <= 0 || altura >= 3){
-    console.log("Altura é inválida");
-    alturaValida = false;
-    tdAltura.textContent = "Altura inválido";
-
-
-}
-
-if(alturaValida  && pesoValido ){
-
-        var imcResultado = ( peso )/(Math.pow( altura ,2));
-
-        var tdImc = paciente.querySelector(".info-imc");
-        tdImc.textContent = imcResultado;
-
-}
-// console.log(paciente); // conter o tr 
-// console.log(tdPeso); // devolver td
-// console.log("O peso é : " + peso); // devolver somente o peso
-// console.log("A altura é : " + altura);// devolver somente o altura
-// console.log("O IMC é : " + imcResultado);// devolver somente o imc
+    // console.log(paciente); // conter o tr 
+    // console.log(tdPeso); // devolver td
+    // console.log("O peso é : " + peso); // devolver somente o peso
+    // console.log("A altura é : " + altura);// devolver somente o altura
+    // console.log("O IMC é : " + imcResultado);// devolver somente o imc
 
 
