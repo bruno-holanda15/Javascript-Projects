40658957f6f42756be2ae124c0639b24c9785c05
bruno-holanda15 <br.holanda15@gmail.com>
2020-02-22 12:56:36 -0300

    Outra forma de adicionar uma data

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -17,13 +17,27 @@ class NegociacaoController{
         // console.log(typeof(this._inputData.value));
         // console.log(this._inputData.value);
 
+        // DESSA MANEIRA, ELE PEGA O TEXTO 2016-11-12 E TRANSFORMA EM UM ARRAY, O SLIPT PEGA O - E SEPARA CADA ELEMENTO DO ARRAY POR ELE,
         let test = this._inputData.value.split('-');
         console.log(test);
 
-        // DESSA MANEIRA, ELE PEGA O TEXTO 2016-11-12 E TRANSFORMA EM UM ARRAY, O SLIPT PEGA O - E SEPARA CADA ELEMENTO DO ARRAY POR ELE,
-        // E A DATE ACEITA ESSE FORMATO PARA CRIAR UM NOVO OBJETO 
-        let data = new Date(this._inputData.value.split('-'));
-        console.log(data);
+        
+        // E A DATE ACEITA ESSE FORMATO PARA CRIAR UM NOVO OBJETO , utilizando o Map para criar um novo array, pois 
+        // a data pega o elemento mes com um decrescimo , como int 11 sendo dec , int 10 nov, por isso no nosso indice 1 do array diminuimos um valor 
+        
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map( function(item , indice) {
+                if (indice == 1 ){
+                    return item - 1;
+                }
+                return item;
+            })
+            
+            );
+        
+            console.log(data);
 
         
 
