7d114245413161b926d95707cdb5f437dbe6a88d
bruno-holanda15 <br.holanda15@gmail.com>
2020-02-22 12:28:13 -0300

    Ajustando o formato da data para criar uma nova Negociacao

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,9 +3,9 @@ class NegociacaoController{
     constructor() {
 
         let $ = document.querySelector.bind(document);
-        this.inputData = $("#data");
-        this.inputQuantidade= $("#quantidade");
-        this.inputValor = $("#valor");
+        this._inputData = $("#data");
+        this._inputQuantidade= $("#quantidade");
+        this._inputValor = $("#valor");
 
     }
 
@@ -13,9 +13,20 @@ class NegociacaoController{
         
         event.preventDefault();
 
-        alert(this.inputQuantidade.value);
-        alert(this.inputValor.value);
+        // DESSE JEITO A DATA NÃO VEM NO FORMATO ADEQUADO PARA CRIAR UMA NOVA DATA
+        // console.log(typeof(this._inputData.value));
+        // console.log(this._inputData.value);
+
+        let test = this._inputData.value.split('-');
+        console.log(test);
+
+        // DESSA MANEIRA, ELE PEGA O TEXTO 2016-11-12 E TRANSFORMA EM UM ARRAY, O SLIPT PEGA O - E SEPARA CADA ELEMENTO DO ARRAY POR ELE,
+        // E A DATE ACEITA ESSE FORMATO PARA CRIAR UM NOVO OBJETO 
+        let data = new Date(this._inputData.value.split('-'));
+        console.log(data);
+
         
+
     }
 
 
