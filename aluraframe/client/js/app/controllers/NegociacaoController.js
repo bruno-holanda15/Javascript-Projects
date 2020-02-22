473457dedb43eb9ea7617bffb001e1aa94e695b0
bruno-holanda15 <br.holanda15@gmail.com>
2020-02-22 13:05:25 -0300

    Otimizando o código usando arrow function

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -18,31 +18,43 @@ class NegociacaoController{
         // console.log(this._inputData.value);
 
         // DESSA MANEIRA, ELE PEGA O TEXTO 2016-11-12 E TRANSFORMA EM UM ARRAY, O SLIPT PEGA O - E SEPARA CADA ELEMENTO DO ARRAY POR ELE,
-        let test = this._inputData.value.split('-');
-        console.log(test);
-
+        // let test = this._inputData.value.split('-');
+        // console.log(test);
         
         // E A DATE ACEITA ESSE FORMATO PARA CRIAR UM NOVO OBJETO , utilizando o Map para criar um novo array, pois 
         // a data pega o elemento mes com um decrescimo , como int 11 sendo dec , int 10 nov, por isso no nosso indice 1 do array diminuimos um valor 
+
+        // let data = new Date(...
+        //     this._inputData.value
+        //     .split('-')
+        //     .map( function(item , indice) {
+        //         if (indice == 1 ){
+        //             return item - 1;
+        //         }
+        //         return item;
+        //     })
+            
+        //     );
         
+        //     console.log(data);
+
+        // Forma resumida , arrow function
         let data = new Date(...
             this._inputData.value
             .split('-')
-            .map( function(item , indice) {
-                if (indice == 1 ){
-                    return item - 1;
-                }
-                return item;
+            .map( (item , indice) => {
+                return item - indice % 2;
             })
-            
-            );
+        );
         
-            console.log(data);
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
 
-        
+        console.log(negociacao);
 
     }
-
-
 
 }
