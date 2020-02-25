93d97f81e5b5a00637d73968e946115dd977621c
bruno-holanda15 <br.holanda15@gmail.com>
2020-02-25 19:36:49 -0300

    Adicionando total na tabela somando os volumes

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -33,6 +33,14 @@ class NegociacoesView{
                 </tbody>
 
                 <tfoot>
+                    <td colspan="3"> </td>
+                    <td> ${
+                        (function() {
+                            let total = 0;
+                            model.negociacoes.forEach( n => total += n.volume);
+                            return total;                               
+                        })()
+                    } </td>
                 </tfoot>
             </table>   
         `;
