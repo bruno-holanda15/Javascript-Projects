4d4c5823b7da60843a29f754c6c3e2c7dceafb04
bruno-holanda15 <br.holanda15@gmail.com>
2020-03-03 21:52:55 -0300

    Adicionando herança para as classes view

--- aluraframe/client/js/app/views/MensagemView.js
+++ aluraframe/client/js/app/views/MensagemView.js
@@ -1,9 +1,9 @@
-class MensagemView{
+class MensagemView extends View{
     
+    //passa para a classe pai o elemento
     constructor(elemento){
-     
-        this._elemento = elemento;
 
+        super(elemento);
     }
 
     _template(model){
@@ -11,8 +11,5 @@ class MensagemView{
         return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : `<p></p>`;
     }
 
-    update(model){
 
-        this._elemento.innerHTML = this._template(model);
-    }
 }
